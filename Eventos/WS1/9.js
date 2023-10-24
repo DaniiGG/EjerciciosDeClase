5d761be2cc974a8cb3092135ac3632fce9a6950b
Daniel Escobar <daniescomoli@gmail.com>
2023-10-24 08:50:31 +0200

    WS1 10

--- Eventos/WS1/9.js
+++ Eventos/WS1/9.js
@@ -1,26 +1,31 @@
 window.onload = function () {
-    const imagenArrastre = document.getElementById('image');
+    const imagenesArrastre = document.querySelectorAll('#image');
     let pegada = false;
+    let imagenSeleccionada = null;
+    let offsetX, offsetY;
 
-    imagenArrastre.addEventListener('click', (e) => {
-        if (pegada) {
-            pegada = false;
-            document.onmousemove = null;
-            
-        } else {
-            pegada = true;
-            var posicionX = e.clientX - imagenArrastre.getBoundingClientRect().left;
-            var posicionY = e.clientY - imagenArrastre.getBoundingClientRect().top;
-            document.onmousemove = (e) => {
-                var nuevoX = e.clientX - posicionX;
-                var nuevoY = e.clientY - posicionY;
-                imagenArrastre.style.left = nuevoX + 'px';
-                imagenArrastre.style.top = nuevoY + 'px';
-            };
-            document.onmouseenter = () => {
+    imagenesArrastre.forEach((imagen) => {
+        imagen.addEventListener('click', (e) => {
+            if (pegada) {
                 pegada = false;
-                 
-            };
-        }
+                document.onmousemove = null;
+                document.onmouseup = null;
+            } else {
+                pegada = true;
+                imagenSeleccionada = imagen;
+                offsetX = e.clientX - imagenSeleccionada.getBoundingClientRect().left;
+                offsetY = e.clientY - imagenSeleccionada.getBoundingClientRect().top;
+
+                document.onmousemove = (e) => {
+                    const nuevoX = e.clientX - offsetX;
+                    const nuevoY = e.clientY - offsetY;
+                    imagenSeleccionada.style.left = nuevoX + 'px';
+                    imagenSeleccionada.style.top = nuevoY + 'px';
+                };
+                document.onmouseenter = () => {
+                    pegada = false;
+                };
+            }
+        });
     });
 };
